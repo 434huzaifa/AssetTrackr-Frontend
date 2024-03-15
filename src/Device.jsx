d0c10b891fee1c3b3346074cbeca16ab75b5b69c
Md. Huzaifa <saadhuzaifa2497@gmail.com>
2024-03-15 12:35:31 +0600

    Checkout form created

--- src/Device.jsx
+++ src/Device.jsx
@@ -8,83 +8,91 @@ import Swal from "sweetalert2";
 import SomethingWrong from "./SomethingWrong";
 
 const Device = () => {
-    const { company, loading } = useAuth();
-    const caxios=useAxios()
-    const [form]=Form.useForm()
-    const queryDevices=useQuery({
-        queryKey:[company?.id,'device'],
-        queryFn:async ()=>{
-            const res= await caxios.get(`device/?id=${company?.id}`)
-            return res.data
-        },
-        enabled:!!company,
-        refetchOnWindowFocus:false,
-        retry:2
-    })
-    const mutationAddDevice= useMutation({
-        mutationFn:async (values)=>{
-            const res= await caxios.post("device/",values)
-            return res.data
-        },
-        onSuccess:(data)=>{
-            SuccessRespons(data)
-            
-        },
-        onError:(err)=>{
-            ErrorResponse(err)
-        }
-    })
-    const mutationDelete=useMutation({
-        mutationFn:async(id)=>{
-            const res=await caxios.delete(`device/${id}/`)
-            return res.data
-        },
-        onSuccess: (data) => {
-            queryDevices.refetch();
-            SuccessRespons(data);
-            
-          },
-          onError: (err) => {
-            ErrorResponse(err);
-          },
-      })
-    async function onFinish(values) {
-        values['company']=company?.id
-        await mutationAddDevice.mutateAsync(values)
-        form.resetFields()
-    }
-    function deleteTheDevice(record) {
-        Swal.fire({
-            title:"Delete",
-            text:`Do you want to delete ${record.name}?`,
-            icon:"question",
-            showCancelButton:true,
-            confirmButtonText:"Yes"
-        }).then(async result=>{
-            if (result.isConfirmed) {
-                await mutationDelete.mutateAsync(record.id)
-            }
-        })
+  const { company, loading } = useAuth();
+  const caxios = useAxios();
+  const [form] = Form.useForm();
+  const queryDevices = useQuery({
+    queryKey: [company?.id, "device"],
+    queryFn: async () => {
+      const res = await caxios.get(`device/?id=${company?.id}`);
+      return res.data;
+    },
+    enabled: !!company,
+    refetchOnWindowFocus: false,
+    retry: 2,
+  });
+  const mutationAddDevice = useMutation({
+    mutationFn: async (values) => {
+      const res = await caxios.post("device/", values);
+      return res.data;
+    },
+    onSuccess: (data) => {
+      queryDevices.refetch();
+      SuccessRespons(data);
+    },
+    onError: (err) => {
+      ErrorResponse(err);
+    },
+  });
+  const mutationDelete = useMutation({
+    mutationFn: async (id) => {
+      const res = await caxios.delete(`device/${id}/`);
+      return res.data;
+    },
+    onSuccess: (data) => {
+      queryDevices.refetch();
+      SuccessRespons(data);
+    },
+    onError: (err) => {
+      ErrorResponse(err);
+    },
+  });
+  async function onFinish(values) {
+    values["company"] = company?.id;
+    await mutationAddDevice.mutateAsync(values);
+    form.resetFields();
+  }
+  function deleteTheDevice(record) {
+    Swal.fire({
+      title: "Delete",
+      text: `Do you want to delete ${record.name}?`,
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "Yes",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await mutationDelete.mutateAsync(record.id);
       }
-    const columns = [
-        {
-          title: "Name",
-          dataIndex: "name",
-          key: "name",
-        },
-        {
-          title: "Condition",
-          dataIndex: "condition",
-          key: "condition",
-        },
-        {
-          title: "Action",
-          key: "action",
-          render: (record) => {
-            return <Button className="bg-red-300 text-red-700" onClick={()=>{deleteTheDevice(record)}}>Delete</Button>;
-          },
-        },
-      ];
+    });
+  }
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Condition",
+      dataIndex: "condition",
+      key: "condition",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (record) => {
+        return (
+          <Button
+            className="bg-red-300 text-red-700"
+            onClick={() => {
+              deleteTheDevice(record);
+            }}
+          >
+            Delete
+          </Button>
+        );
+      },
+    },
+  ];
   return (
     <div className="mx-28 mt-6">
       <Card loading={loading || mutationAddDevice.isPending}>
@@ -98,13 +106,13 @@ const Device = () => {
             <Input></Input>
           </Form.Item>
           <Form.Item
-           label="Condition"
-           name="condition"
-           validateTrigger="onBlur"
-           rules={[{ required: true }]}
+            label="Condition"
+            name="condition"
+            validateTrigger="onBlur"
+            rules={[{ required: true }]}
           >
             <Select
-            allowClear
+              allowClear
               options={[
                 { value: "Excellent", label: "Excellent" },
                 { value: "Good", label: "Good" },
@@ -119,9 +127,10 @@ const Device = () => {
       <Card
         className="mt-3"
         loading={
-            queryDevices.isLoading ||
-            queryDevices.isRefetching ||
-            queryDevices.isFetching || mutationDelete.isPending
+          queryDevices.isLoading ||
+          queryDevices.isRefetching ||
+          queryDevices.isFetching ||
+          mutationDelete.isPending
         }
       >
         {queryDevices.isSuccess ? (
@@ -129,7 +138,7 @@ const Device = () => {
             <Table
               columns={columns}
               pagination={false}
-              rowKey={(record)=>(record.id)}
+              rowKey={(record) => record.id}
               bordered={true}
               dataSource={queryDevices.data?.devices}
             ></Table>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,91 @@
-import { Spin } from "antd";
+import { Button, Card, DatePicker, Form, Select, Spin } from "antd";
 import useAuth from "./useAuth";
+import { useQuery } from "@tanstack/react-query";
+import useAxios from "./useAxios";
+import dayjs from "dayjs";
 
 const Home = () => {
-    const {company,loading}=useAuth()
-    
-    return (
-        <Spin spinning={loading}>
-            
-        </Spin>
-    );
+  const { company, loading } = useAuth();
+  const [form] = Form.useForm();
+  const caxios = useAxios();
+  const queryCheckoutInfo = useQuery({
+    queryKey: ["checkoutinfo", company?.id],
+    queryFn: async () => {
+      const res = await caxios.get(`checkout-info/?id=${company?.id}`);
+      return res.data;
+    },
+    refetchOnWindowFocus: false,
+    retry: 2,
+    enabled: !!company,
+  });
+  function onFinish(values) {
+      values["return_date"]=values.return_date.format("DD-MM-YYYY")
+      console.log(values);
+  }
+  return (
+    <div className="mx-28 mt-6">
+      <Card loading={loading || queryCheckoutInfo.isLoading}>
+        <Form layout="vertical" form={form} onFinish={onFinish}>
+          <Form.Item
+            label="Device"
+            name="device"
+            rules={[{ required: true }]}
+            validateTrigger="onBlur"
+          >
+            <Select
+              allowClear
+              options={queryCheckoutInfo.data?.devices.map((x) => {
+                return { value: x.id, label: x.name };
+              })}
+            ></Select>
+          </Form.Item>
+          <Form.Item
+            label="Employee"
+            name="employee"
+            rules={[{ required: true }]}
+            validateTrigger="onBlur"
+          >
+            <Select
+              allowClear
+              options={queryCheckoutInfo.data?.employees.map((x) => {
+                return { value: x.id, label: x.name };
+              })}
+            ></Select>
+          </Form.Item>
+          <Form.Item
+            label="Return Date"
+            name="return_date"
+            rules={[{ required: true }]}
+            validateTrigger="onBlur"
+          >
+            <DatePicker
+            allowClear
+              className="w-full"
+              minDate={dayjs()}
+              format="DD-MM-YYYY"
+            />
+          </Form.Item>
+          <Form.Item
+            label="Checkout Condition"
+            name="checkout_condition"
+            rules={[{ required: true }]}
+            validateTrigger="onBlur"
+          >
+            <Select
+              allowClear
+              options={[
+                { value: "Excellent", label: "Excellent" },
+                { value: "Good", label: "Good" },
+                { value: "Fair", label: "Fair" },
+                { value: "Poor", label: "Poor" },
+              ]}
+            ></Select>
+          </Form.Item>
+          <Button htmlType="submit">Checkout</Button>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default Home;
